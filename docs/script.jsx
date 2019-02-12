0cbf8850bf9dc52e38dad98d8bf127fc64497c02
mitchellgsides <mitchellgsides@gmail.com>
2019-02-12 16:48:46 -0600

    testing oauth functionality for users

--- docs/script.jsx
+++ docs/script.jsx
@@ -68,40 +68,7 @@ console.log(normPow(responseObject.watts.data, 500, 600));
 
 /*Block for Oauth and Implementation of List Activities
 
-
-
-
-
-
-
-
-
-
-
+Below is HOW I'll grant access and then gather data for the app.
 
 */
-var StravaApiV3 = require('strava_api_v3');
-var defaultClient = StravaApiV3.ApiClient.instance;
-
-// Configure OAuth2 access token for authorization: strava_oauth
-var strava_oauth = defaultClient.authentications['strava_oauth'];
-strava_oauth.accessToken = "YOUR ACCESS TOKEN"
-
-var api = new StravaApiV3.ActivitiesApi()
-
-var opts = { 
-  'before': 56, // {Integer} An epoch timestamp to use for filtering activities that have taken place before a certain time.
-  'after': 56, // {Integer} An epoch timestamp to use for filtering activities that have taken place after a certain time.
-  'page': 56, // {Integer} Page number.
-  'perPage': 56 // {Integer} Number of items per page. Defaults to 30.
-};
-
-var callback = function(error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('API called successfully. Returned data: ' + data);
-  }
-};
-api.getLoggedInAthleteActivities(opts, callback);
 
